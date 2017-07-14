9f674a7f04b3469feb88bfcd2cecb4ea1a2fb958
DennisKimani <dennisngugi.dk@gmail.com>
2017-07-14 10:41:41 +0300

    added some styling to my home page added a function which will enable to count words which are having punctuations and upcase words as one

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -2,10 +2,9 @@ class String
   define_method(:count) do |word_to_count|
     counter = 0
     split = self.split()
-    split.each() do |word|
-      if word==word_to_count
-        counter+=1
-      end
+    split.each do |word|
+      word_to_count = word.gsub(/[!,@,?,"",:,;,.]/, '')
+      counter += 1 if word == word_to_count
     end
     counter
   end
